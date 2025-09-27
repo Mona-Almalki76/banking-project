3ad1eff80a74c9f9efeb9de38fa580ef8342dd0e
mona-almalki <mona.fawazalmalki76@gmail.com>
2025-09-27 19:29:46 +0300

    Add transfer to another customer unit test

--- tests/test_bank_transfer.py
+++ tests/test_bank_transfer.py
@@ -7,6 +7,11 @@ class TestTransfer(unittest.TestCase):
         self.c1 = self.bank.add_customer("Mona", "Almalki", "pass123")
         self.c1.balance_checking = 500
         self.c1.balance_savings = 300
+
+        self.c2 = self.bank.add_customer("Sara", "Ali", "word321")
+        self.c2.balance_checking = 200
+        self.c2.balance_savings = 100
+
         self.bank.login(self.c1.account_id, "pass123")
 
     def test_transfer_savings_to_checking(self):
@@ -24,6 +29,12 @@ class TestTransfer(unittest.TestCase):
     def test_transfer_insufficient_funds(self):
         with self.assertRaises(ValueError):
             self.bank.transfer(1000, "checking", "savings")
+
+    def test_transfer_to_another_customer(self):
+        txn = self.bank.transfer(100, "checking", "checking", recipient_account_id=self.c2.account_id)
+        self.assertEqual(self.c1.balance_checking, 400)
+        self.assertEqual(self.c2.balance_checking, 300)
+        self.assertEqual(txn.status, "SUCCESS")
 
 if __name__ == "__main__":
     unittest.main()
